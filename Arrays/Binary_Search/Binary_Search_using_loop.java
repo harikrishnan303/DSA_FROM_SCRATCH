9bd779caf60d779b3b4c42cf68439d1f83800a05
harikrishnan303 <harikrishnanaruchsamy@gmail.com>
2025-09-26 12:15:04 +0530

    Again day 1 started

--- Arrays/Binary_Search/Binary_Search_using_loop.java
+++ Arrays/Binary_Search/Binary_Search_using_loop.java
@@ -20,3 +20,28 @@ class Solution {
         return -1;
     }
 }
+
+Recursive Approach
+class Solution {
+    public int search(int[] nums, int target) 
+    {
+        return binarysearch(nums,target,0,nums.length-1);
+    }
+    public int binarysearch(int[] nums,int target,int start,int end)
+    {
+        if(start>end)
+        return -1;
+        
+        int mid=(start+end)/2;
+        if(nums[mid]==target)
+        return mid;
+        else if(nums[mid]<target)
+        {
+            return binarysearch(nums,target,mid+1,end);
+        }
+        else
+        {
+            return binarysearch(nums,target,start,mid-1);
+        }
+    }
+}
